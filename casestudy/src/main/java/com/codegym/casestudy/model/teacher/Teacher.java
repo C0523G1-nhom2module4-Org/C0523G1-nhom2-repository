9330dff144c13483611db568bd8b71473ce86219
Nguyen Thi My Hanh <131949097+myhanhhhhh@users.noreply.github.com>
2023-09-30 22:24:43 +0700

    up

--- casestudy/src/main/java/com/codegym/casestudy/model/teacher/Teacher.java
+++ casestudy/src/main/java/com/codegym/casestudy/model/teacher/Teacher.java
@@ -1,5 +1,9 @@
 package com.codegym.casestudy.model.teacher;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,18 +11,21 @@ import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Teacher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String identity;
-    private boolean gender;
-    private LocalDate birthday ;
+    private int gender;
+    private String  birthday ;
     private String phoneNumber;
-    private double point;
+    private int point;
     private String address;
-    private double salary;
-    private boolean status;
+    private int salary;
+    private int status;
 
 }
