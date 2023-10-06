67786e9d0e02bc3d3b2c98ed603c4b30e130511a
baothienc0523g1 <noimandembuongxuong@gmail.com>
2023-10-06 10:23:18 +0700

    khoa hoc + lien ket khoa hoc

--- casestudy/src/main/java/com/codegym/casestudy/controller/main_page_controller/MainPageController.java
+++ casestudy/src/main/java/com/codegym/casestudy/controller/main_page_controller/MainPageController.java
@@ -1,19 +1,29 @@
 package com.codegym.casestudy.controller.main_page_controller;
 
+import com.codegym.casestudy.model.qualification.Qualification;
+import com.codegym.casestudy.service.qualification.IQualificationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class MainPageController {
+    @Autowired
+    private IQualificationService qualificationService;
     @GetMapping()
     public String index() {
         return "/index";
     }
 
     @GetMapping("/feature")
-    public String feature() {
+    public String feature(Model model) {
+        List<Qualification> qualificationList = this.qualificationService.findAll();
+        model.addAttribute("qualificationList", qualificationList);
         return "/feature";
     }
 
